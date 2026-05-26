e19386dc24ceae67fe024b73f142cc2e3c097100
sjsutton27 <ssutton101600@gmail.com>
2026-05-25 20:51:04 -0400

    Set up database

---
+++ app/src/main/java/com/example/myapplication/data/data_source/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.myapplication.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Query(value = "SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query(value = "SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
---
+++ app/src/main/java/com/example/myapplication/domain/model/Note.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.myapplication.ui.theme.BabyBlue
+import com.example.myapplication.ui.theme.LightGreen
+import com.example.myapplication.ui.theme.RedOrange
+import com.example.myapplication.ui.theme.RedPink
+import com.example.myapplication.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+){
+    companion object{
+        val noteColor = listOf(
+            RedOrange,
+            LightGreen,
+            Violet,
+            BabyBlue,
+            RedPink
+        )
+    }
+}
